f75392bf62b30138847655aa868682de562e96ef
Jason Miller <jason@marque360.com>
2026-01-09 16:40:27 -0600

    pepper

--- src/pyAvStreamer.py
+++ src/pyAvStreamer.py
@@ -109,7 +109,7 @@ def stream_audio_task(pyaudio_instance, device_index, device_name, port, stop_ev
     Worker function to stream audio from a specific device to a UDP port.
     """
     print(f"[Audio] Stream for '{device_name}' starting...")
-    print(f" - srt://@:{port?mode=listener&latency=50000")
+    print(f" - srt://@:{port}?mode=listener&latency=50000")
 
     FFMPEG_BIN = get_ffmpeg_path()
     if not FFMPEG_BIN:
